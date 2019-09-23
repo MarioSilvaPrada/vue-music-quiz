cd7a5d0baa6bdcb7ea0c4fde3ff1c58de73fd84f
Mário Prada <mariosilvaprada@gmail.com>
2019-09-23 23:43:42 +0100

    updating

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,60 @@
 <template>
-    <h1>QuestionBox</h1>
+   <div>
+        <b-jumbotron >
+
+        <template v-slot:lead>
+        {{currentQuestion.question}}
+        </template>
+
+        <hr class="my-4">
+
+        <b-list-group >
+            <b-list-group-item 
+            v-for="(answer, index) of answers" 
+            :key="index"
+            @click="selectAnswer(index)">
+                {{answer}}
+            </b-list-group-item>
+        </b-list-group>
+      
+        <b-button variant="primary" href="#">Submit</b-button>
+        <b-button @click="next" variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
 </template>
+
+<script>
+    export default {
+        props: {
+            currentQuestion: Object,
+            next: Function
+        },
+        data() {
+            return {
+                selectedIndex: null
+            }
+        },
+        computed: {
+            answers() {
+                let answers = [...this.currentQuestion.incorrect_answers]
+                answers.push(this.currentQuestion.correct_answer)
+                return answers
+            }
+        },
+        methods: {
+            selectAnswer(index) {
+                this.selectedIndex = index
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .list-group {
+        margin-bottom: 15px;
+        cursor: pointer;
+    }
+    .btn {
+        margin: 0 5px;
+    }
+</style>
